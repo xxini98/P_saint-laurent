717d5bb9a1399cb0bc6bda4b332bc1c3f55d6907
zzining <iammes98@gmail.com>
2023-10-11 17:52:49 +0900

    setting

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -11,3 +11,20 @@ var swiper = new Swiper(".slide-intro", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+var sidebar = new StickySidebar(".sidebar", {
+  topSpacing: 20,
+  bottomSpacing: 20,
+  containerSelector: ".wrap",
+  innerWrapperSelector: ".sidebar__inner",
+});
+
+$(function () {
+  $(".depth1 > li").mouseover(function () {
+    $(this).find(".depth2").stop().slideDown(400);
+    $(this).addClass("on");
+  });
+  $(".depth1 > li").mouseleave(function () {
+    $(this).find(".depth2").stop().slideUp(400);
+  });
+});
